2133f4c4a2c0efa72b60dfca1bf4103e95a095cc
Anastasia <48135406+MezentsevaAnastasia@users.noreply.github.com>
2021-06-07 01:20:40 +0700

    Docs&typing  (#2)

--- evaluator.py
+++ evaluator.py
@@ -4,6 +4,7 @@ from rouge_metric import PyRouge
 
 
 class Evaluator:
+    """Class for calculating metrics"""
 
     def __init__(self):
         pass
@@ -12,13 +13,18 @@ class Evaluator:
         """
         :param: hypotheses - list of documents not tokenized
         references - List[doc1[ref1[sent1[words]], ref2], doc2[ref1, ref2]]
-        :return:
+        :return: values for ROUGE metric
         """
         rouge = PyRouge(rouge_n=(1, 2, 3), rouge_l=True, rouge_w=False, skip_gap=4)
         scores = rouge.evaluate_tokenized(self._tokenize(hypotheses), self._tokenize(references))
         return scores
 
-    def _tokenize(self, document):
+    def _tokenize(self, document: List[str]):
+        """
+        Tokenize sentences in Russian
+        :param document: list of sentences
+        :return: list of tokenized sentences
+        """
         tokenized_sents = list()
         for sent in document:
             tokenized_sents.append(word_tokenize(sent, language="russian"))
